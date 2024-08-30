212c9501c1ec7b03106251ae6fa49c2a25ec87f1
akhil.abraham <akhil.abraham@qburst.com>
2024-08-30 07:46:03 +0530

    test commit

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const express = require("express");
 const server = express();
 const http = require("http");
+const passport = require("passport/lib");
 const SamlStrategy = require("@node-saml/passport-saml").Strategy;
 
-passport.use(
-  new SamlStrategy(
-    {
-      issuer: sp.issuer,
-      callbackUrl: sp.callbackUrl,
-      entryPoint: idp.signonUrl,
-      logoutUrl: idp.logoutUrl,
-      cert: idp.cert,
-      disableRequestedAuthnContext: true,
-      privateCert: readFileSync(CONFIG.authentication.privateCert, "utf-8"),
-      acceptedClockSkewMs: -1,
-      identifierFormat: null,
-      signatureAlgorithm: "sha256",
-    },
-    function (profile, done) {
-      // for signon
-      console.log(profile);
-    },
-    function (profile, done) {
-      // for logout
-      findByNameID(profile.nameID, function (err, user) {
-        if (err) {
-          return done(err);
-        }
-        return done(null, user);
-      });
-    }
-  )
-);
+// passport.use(
+//   new SamlStrategy(
+//     {
+//       issuer: "test",
+//       callbackUrl: "http://",
+//       entryPoint: "",
+//       logoutUrl: "",
+//       cert: "test",
+//       disableRequestedAuthnContext: true,
+//       privateCert: "test",
+//       acceptedClockSkewMs: -1,
+//       identifierFormat: null,
+//       signatureAlgorithm: "sha256",
+//     },
+//     function (profile, done) {
+//       // for signon
+//       console.log(profile);
+//     },
+//     function (profile, done) {
+//       // for logout
+//       findByNameID(profile.nameID, function (err, user) {
+//         if (err) {
+//           return done(err);
+//         }
+//         return done(null, user);
+//       });
+//     }
+//   )
+// );
 
 const httpServer = http.createServer(server);
 httpServer.listen("3000", () => {
